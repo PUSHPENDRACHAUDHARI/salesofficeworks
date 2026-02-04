bae8a108b7d885bca5e06157a7ff735f8f28a964
root <root@srv1222645.hstgr.cloud>
2026-02-04 09:03:05 +0000

    update login.component.ts

--- salesmanagement1/src/app/login/login.component.ts
+++ salesmanagement1/src/app/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent {
   message = '';
   isError = false;
 
-  private baseUrl = 'http://31.97.203.68:9081/';
+  private baseUrl = 'http://31.97.203.68:9092';
 
   constructor(private http: HttpClient, private router: Router) {}
 
