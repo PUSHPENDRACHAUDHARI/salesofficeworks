dbd223eaaf3aa7656cf353422ed2886256988d34
root <root@srv1222645.hstgr.cloud>
2026-02-04 09:23:56 +0000

    add cross origin

---
+++ management/src/main/java/com/sales/management/controller/HealthController.java
@@ -0,0 +1,15 @@
+
+package com.sales.management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Backend is running successfully";
+    }
+}
+
