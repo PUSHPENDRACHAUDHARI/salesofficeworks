4611442eb0114f1f37a8a3ae465ebb4571056ec9
Pushpendra-Chaudhari <139942250+PUSHPENDRACHAUDHARI@users.noreply.github.com>
2026-02-04 15:48:38 +0530

    Remove CrossOrigin annotation from LeadsController
    Removed CrossOrigin annotation for LeadsController.

--- management/src/main/java/com/sales/management/controller/LeadsController.java
+++ management/src/main/java/com/sales/management/controller/LeadsController.java
@@ -13,7 +13,6 @@ import com.sales.management.entity.LeadsEntity;
 import com.sales.management.entity.TaskEntity;
 
 @RestController
-@CrossOrigin(origins = "http://31.97.203.68:9081/")
 public class LeadsController {
 
     @Autowired
