112de6b403166a7b03cd0002de8cda83bd5d142a
Pushpendra-Chaudhari <139942250+PUSHPENDRACHAUDHARI@users.noreply.github.com>
2026-02-04 15:27:08 +0530

    Add CrossOrigin annotation to UserDashboardController

--- management/src/main/java/com/sales/management/controller/UserDashboardController.java
+++ management/src/main/java/com/sales/management/controller/UserDashboardController.java
@@ -15,6 +15,7 @@ import com.sales.management.Repository.ProjectRepository;
 import com.sales.management.dto.ApiResponse;
 
 @RestController
+@CrossOrigin(origins = "http://31.97.203.68:9081/")
 @RequestMapping("/userdashboard")
 public class UserDashboardController {
 
