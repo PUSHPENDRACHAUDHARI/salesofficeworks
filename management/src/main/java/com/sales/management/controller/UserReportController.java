ffd84f4dde7853b3def5177edbeb618335c63887
Pushpendra-Chaudhari <139942250+PUSHPENDRACHAUDHARI@users.noreply.github.com>
2026-02-04 15:31:20 +0530

    Update CrossOrigin annotation to remove trailing slash

--- management/src/main/java/com/sales/management/controller/UserReportController.java
+++ management/src/main/java/com/sales/management/controller/UserReportController.java
@@ -12,7 +12,7 @@ import com.sales.management.dto.ApiResponse;
 import com.sales.management.dto.UserDailyReportDto;
 
 @RestController
-@CrossOrigin(origins = "http://31.97.203.68:9081/")
+@CrossOrigin(origins = "http://31.97.203.68:9081")
 @RequestMapping("/user/report")
 public class UserReportController {
 
